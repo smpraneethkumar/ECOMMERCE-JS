7f93de889244aacce56947ec8a96db1c172020ac
sm praneeth kumar <smparneethkumr431gmail.com>
2025-02-06 12:53:51 +0530

    cart empty banner is done

---
+++ cart.js
@@ -0,0 +1,12 @@
+
+
+let defDisplay = `
+<div class="container">
+    <h1>Cart</h1>
+    <hr> <br><br><br>
+    <h1 class="display-1"> Your Cart is Empty</h1>
+    <button type="button" class="btn btn-outline-dark"><a class="nav-link active" aria-current="page" href="product.html"> Continue Shopping</a></button>
+
+</div>`
+
+console.log(defDisplay);
