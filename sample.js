82ca71d0c2264231dabf12816229eaae2e3aab60
sm praneeth kumar <smparneethkumr431gmail.com>
2025-02-07 21:11:38 +0530

    not given function for add to cart button

--- sample.js
+++ sample.js
@@ -1,5 +1,9 @@
 let row = document.querySelector(".row");
 
+
+
+
+
 let data1 = []
 
 async function fetchData (){
@@ -12,49 +16,42 @@ async function fetchData (){
         })
     }
     fetchData()
-   
-    
-
-    // for (const key in object) {
-    //     if (Object.prototype.hasOwnProperty.call(object, key)) {
-    //         const element = object[key];
-            
-    //     }
-    // }
-
-
+  
     function createcards(source){
       var image =""
     source.forEach((currObj) => {
       let x =  `
-       "<div class="card  col-4" style="width: 18rem; display:flex; margin:7px;text-align: center;">
-      <img src="${currObj.image}" class="card-img-top" alt="iamges is not getting">
-      <div class="card-body">
+       <div class="card  col-4" style="width: 50px;  display:grid; margin:7px;text-align: center;">
+      <img src="${currObj.image}"alt="iamges is not getting">
+         
         <h5 class="card-title" style="overflow:hidden; text-overflow:ellipsis; white-space:nowrap;">${currObj.title}</h5>
         <p class="card-text" style="overflow:hidden; text-overflow:ellipsis; white-space:nowrap;">${currObj.description}</p><hr>
         <p>${currObj.price} </p>
       </div><hr>
       <div class="card-body">
-       <button type="button" class="btn btn-dark details">Details</button>
+       <button type="button" class="btn btn-dark details " data-id="${currObj.id}">Details</button>
        <button type="button" class="btn btn-dark">Add to Cart</button>
       </div>
     </div>`
     image+=x;
   })
       row.innerHTML=image;
-}
+      let detailsBtn = document.querySelectorAll(".details")
+      detailsBtn.forEach((deBtn)=>{
+        deBtn.addEventListener("click",(e)=>{
+           let id = e.target.dataset.id 
+            console.log(e);
+           
+                 localStorage.setItem("id",id)
+                 console.log(localStorage.getItem("id"));          
+                window.location.href="./product.html";
+                })
+        
+              })
 
-   
-  // let dertrailbtn = document.querySelectorAll(".details")
-  
-  
-console.log("fsddfsdfs");
+ }
 
 
-  
-   
-
- 
     
 
     // for all button
@@ -113,6 +110,13 @@ let menbtn = document.getElementById("men");
     // let dbtn = `${data1.title}`
 
     // console.log(dbtn);
+
+
+    let productpage = document.getElementById("productpage")
+
+productpage.addEventListener("click",(e)=>{
+    localStorage.setItem("id",e.target.dataset.id)
+})
     
     
    
